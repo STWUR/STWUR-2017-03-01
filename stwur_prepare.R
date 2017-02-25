d1d79b3cd1210922c02b0216ef23ef1293038872
michbur <michalburdukiewicz@gmail.com>
2017-02-25 11:12:47 +0100

    added prepare script

--- stwur_prepare.R
+++ stwur_prepare.R
@@ -1,1 +1,5 @@
-install.packages("shiny", repos = "https://cran.rstudio.com/")
+install.packages("devtools", repos = "https://cloud.r-project.org/")
+devtools::install_github("rstudio/tutor")
+download.file(url = "https://raw.githubusercontent.com/STWUR/STWUR-2017-03-01/master/tutorial_wizualizacje.Rmd", 
+              destfile = "tutorial_wizualizacje.Rmd")
+#rmarkdown::run("tutorial_wizualizacje.Rmd")
